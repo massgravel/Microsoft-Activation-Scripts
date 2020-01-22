b65004daf0a8b44b42e7a8a5939995fea7c41c78
WindowsAddict <windowsaddict@protonmail.com>
2020-01-22 06:05:20 +0530

    Add files via upload
    MAS 1.3
    
    # HWID / KMS38
    
     - Fixed a bug in Enterprise Edition activation.
     - Updated the ticket generation and applying process.
    
    # All
    
     - Added a project mirror on github.
     - Some minor improvements

---
+++ MAS_1.3/Separate-Files-Version/Homepages.cmd
@@ -0,0 +1,4 @@
+@echo off
+start https://www.nsaneforums.com/topic/316668-microsoft-activation-scripts/ &:: Login required
+start https://gitlab.com/massgrave/microsoft-activation-scripts
+start https://github.com/massgravel/Microsoft-Activation-Scripts
