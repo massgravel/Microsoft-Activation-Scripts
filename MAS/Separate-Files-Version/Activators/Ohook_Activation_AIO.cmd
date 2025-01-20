0c88f6bc2c6b730ab2512bc5368173ea89c44657
WindowsAddict <Windowsaddict@protonmail.com>
2025-01-20 22:35:47 +0530

    Change error detection for ImageState

--- MAS/Separate-Files-Version/Activators/Ohook_Activation_AIO.cmd
+++ MAS/Separate-Files-Version/Activators/Ohook_Activation_AIO.cmd
@@ -1672,10 +1672,10 @@ call :dk_color2 %Red% "Checking Boot Mode                      [%safeboot_option
 for /f "skip=2 tokens=2*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State" /v ImageState') do (set imagestate=%%B)
 
 if /i not "%imagestate%"=="IMAGE_STATE_COMPLETE" (
-set error=1
-set showfix=1
 call :dk_color %Gray% "Checking Windows Setup State            [%imagestate%]"
 echo "%imagestate%" | find /i "RESEAL" %nul% && (
+set error=1
+set showfix=1
 call :dk_color %Blue% "You need to run it in normal mode in case you are running it in Audit Mode."
 )
 echo "%imagestate%" | find /i "UNDEPLOYABLE" %nul% && (
